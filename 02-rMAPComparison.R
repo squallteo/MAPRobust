b88ca210be27042f1ce510331c802ae49c2bfdc5
squallteo <49884198+squallteo@users.noreply.github.com>
2019-07-03 18:21:03 -0500

    Updated 2019-07-03

--- 02-rMAPComparison.R
+++ 02-rMAPComparison.R
@@ -5,36 +5,59 @@ library(RBesT)
 library(ggplot2)
 source("00-BhattacharyyaDistance.R")
 
+
+#historical control data
 dt = crohn
 sigma = 88
 dt$se_yh = sigma/sqrt(dt$n)
-#historical control data
 yh = dt$y
 nh = dt$n
+
+#meta analysis of historical data
+# with(dt, meta::metamean(n = n, mean = y, sd = rep(sigma,length(study))))
+#decide to consider true current mean from -60 to -40
+
 #current control data
-y_c = -76.01
-se_yc = 21.93
-n_c = 20
-#robustification parameter: the weight associated with the robust part
-w_r = 0.2
-robust_sd = 200
+n_c = 20 #current control sample size
+muvec_c = seq(-60, -40, by=2) #current control mean vector
 
 #control arm hyper-parameters
 se_mu_c = 100
 HNscale_c = 44
 
+#robustification parameter: the weight associated with the robust part
+w_r = 0.2
+robust_sd = 200
+
 #current treatment data/parameters
 se_mu_t = 100
 HNscale_t = 10000
-y_t = -29.15
-se_yt = 16.69
-n_t = 39
+n_t = 40
+effsize = 15
 
 #MCMC control parameters
 n.chains = 3
 nsim = 10
 
-distance = dur_jags = dur_rbest = rep(NA,length=nsim)
+#initialization at the control mean level
+
+
+for(m in 1:length(muvec_c)){
+  #simulate current control data
+  sample_c = rnorm(n_c, muvec_c[m], sigma)
+  y_c = mean(sample_c)
+  se_yc = sd(sample_c)/sqrt(n_c)
+  
+  #simulate current treatment data
+  sample_t = rnorm(n_t, muvec_c[m] + effsize, sigma)
+  y_t = mean(sample_t)
+  se_yt = sd(sample_t)/sqrt(n_t)
+  
+  #initialization at the simulation level
+  distance = dur_jags = dur_rbest = rep(NA,length=nsim)
+  
+}
+
 
 for(s in 1:nsim){
   print(s)
@@ -48,13 +71,15 @@ for(s in 1:nsim){
   )
   jags_obj = jags(model.file = "Model_NormalHN.bugs",
                   data = jags_data,
-                  parameters.to.save = c("mu_c"),
+                  parameters.to.save = c("mu_c","mu_t"),
                   n.chains = n.chains, n.burnin = 2000, n.iter = 10000,
                   progress.bar = "none"
   )
   
   jags_auto = autojags(jags_obj, Rhat = 1.1, n.thin = 4, n.iter = 40000, n.update = 10, progress.bar = "none")
-  postdt_jags_b = data.frame(jags_auto$BUGSoutput$sims.matrix)$mu_c
+  tt = data.frame(jags_auto$BUGSoutput$sims.matrix)
+  post_c_jags_b = tt$mu_c
+  post_t_jags = tt$mu_t
   
   #robust model with a fixed tau
   jags_data = with(dt,list(prec_mu_c = se_mu_c^-2, prec_c = robust_sd^-2, y_c = y_c, prec_yc = se_yc^-2, yh = yh, prec_yh = se_yh^-2, 
@@ -68,14 +93,18 @@ for(s in 1:nsim){
   )
   
   jags_auto = autojags(jags_obj, Rhat = 1.1, n.thin = 4, n.iter = 40000, n.update = 10, progress.bar = "none")
-  postdt_jags_nb = data.frame(jags_auto$BUGSoutput$sims.matrix)$mu_c
+  post_c_jags_nb = data.frame(jags_auto$BUGSoutput$sims.matrix)$mu_c
   
   #model averaging
-  postdt_jags = (1 - w_r) * postdt_jags_b + w_r * postdt_jags_nb
+  post_c_jags = (1 - w_r) * post_c_jags_b + w_r * post_c_jags_nb
   
   end = Sys.time()
-  
   dur_jags[s] = as.numeric(end - start)
+  
+  #posterior difference
+  post_diff_jags = post_c_jags - post_t_jags
+  
+  
   ##########################################################
   ##########################################################
   ##########################################################
