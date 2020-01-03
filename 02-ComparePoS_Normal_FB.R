105d8492066e512e9c88d060b6d73d3bfc3ec8c5
Hongtao <49884198+squallteo@users.noreply.github.com>
2020-01-03 12:24:00 -0500

    Updated 2020-01-03

--- 02-ComparePoS_Normal_FB.R
+++ 02-ComparePoS_Normal_FB.R
@@ -7,7 +7,7 @@ library(doParallel)
 source("00-BhattacharyyaDistance.R")
 source("02-NormalSimSpec1.R")
 
-nsim = 500
+nsim = 1000
 ncores = parallel::detectCores()
 cl = makeCluster(ncores)
 registerDoParallel(cl)
@@ -49,9 +49,9 @@ for(m in 1:length(muvec_c)){
       y_t = mean(sample_t)
       se_yt = sd(sample_t)/sqrt(n_t)
       
-      ######################
-      #Robust mixture prior#
-      ######################
+      #######################
+      #Full Bayesian Mixture#
+      #######################
       jags_data = with(dt,list(prec_mu_c = se_mu_c^-2, HNscale_c = HNscale_c, y_c = y_c, prec_yc = se_yc^-2, yh = yh, prec_yh = se_yh^-2,
                                prec_mu_t = se_mu_t^-2,                        y_t = y_t, prec_yt = se_yt^-2,
                                w_r = w_r, robust_sd = robust_sd)
@@ -64,28 +64,70 @@ for(m in 1:length(muvec_c)){
       )
       jags_auto = autojags(jags_obj, Rhat = 1.1, n.thin = 4, n.iter = 40000, n.update = 10, progress.bar = "none")
       tt = data.frame(jags_auto$BUGSoutput$sims.matrix)
-      post_c_jags = tt$mu_c
-      post_t_jags = tt$mu_t
+      post_c_fb = tt$mu_c
+      post_t_fb = tt$mu_t
       
       #posterior difference
-      post_diff_jags = post_t_jags - post_c_jags
+      post_diff_fb = post_t_fb - post_c_fb
+      decision_fb = (mean(post_diff_fb <= Qcut) > Pcut)      
+      ######################
+      #Robust mixture prior#
+      ######################
+      #original MAP model
+      jags_data = with(dt,list(prec_mu_c = se_mu_c^-2, HNscale_c = HNscale_c, y_c = y_c, prec_yc = se_yc^-2, yh = yh, prec_yh = se_yh^-2,
+                               prec_mu_t = se_mu_t^-2,                        y_t = y_t, prec_yt = se_yt^-2)
+      )
+      jags_obj = jags(model.file = "Model_NormalHN.bugs",
+                      data = jags_data,
+                      parameters.to.save = c("mu_c","mu_t"),
+                      n.chains = n.chains, n.burnin = 2000, n.iter = 10000,
+                      progress.bar = "none"
+      )
       
-      decision_jags = (mean(post_diff_jags <= Qcut) > Pcut)
+      jags_auto = autojags(jags_obj, Rhat = 1.1, n.thin = 4, n.iter = 40000, n.update = 10, progress.bar = "none")
+      tt = data.frame(jags_auto$BUGSoutput$sims.matrix)
+      post_c_rmix_b = tt$mu_c
+      post_t_rmix = tt$mu_t
       
+      #robust model with a fixed tau
+      jags_data = with(dt,list(prec_mu_c = se_mu_c^-2, prec_c = robust_sd^-2, y_c = y_c, prec_yc = se_yc^-2, yh = yh, prec_yh = se_yh^-2,
+                               prec_mu_t = se_mu_t^-2,                        y_t = y_t, prec_yt = se_yt^-2)
+      )
+      jags_obj = jags(model.file = "Model_NormalFixedTau.bugs",
+                      data = jags_data,
+                      parameters.to.save = c("mu_c"),
+                      n.chains = n.chains, n.burnin = 2000, n.iter = 10000,
+                      progress.bar = "none"
+      )
+      
+      jags_auto = autojags(jags_obj, Rhat = 1.1, n.thin = 4, n.iter = 40000, n.update = 10, progress.bar = "none")
+      post_c_rmix_nb = data.frame(jags_auto$BUGSoutput$sims.matrix)$mu_c
+      
+      #model averaging
+      post_c_rmix = (1 - w_r) * post_c_rmix_b + w_r * post_c_rmix_nb
+      
+      #posterior difference
+      post_diff_rmix = post_t_rmix - post_c_rmix
+      decision_rmix = (mean(post_diff_rmix <= Qcut) > Pcut)
+      
+      ######
+      #rMAP#
+      ######
       #get the posterior mixtures
       postmix_c = postmix(rmap_c,m = y_c, se = se_yc)
       postmix_t = postmix(prior_t, m = y_t, se = se_yt)
       
-      post_c_rbest = rmix(mix = postmix_c, n = length(post_diff_jags))
-      post_t_rbest = rmix(mix = postmix_t, n = length(post_diff_jags))
+      post_c_rmap = rmix(mix = postmix_c, n = length(post_diff_fb))
+      post_t_rmap = rmix(mix = postmix_t, n = length(post_diff_fb))
       
-      post_diff_rbest = post_t_rbest - post_c_rbest
-      decision_rbest = (mean(post_diff_rbest <= Qcut) > Pcut)
+      post_diff_rmap = post_t_rmap - post_c_rmap
+      decision_rmap = (mean(post_diff_rmap <= Qcut) > Pcut)
       
-      c(decision_jags,decision_rbest)
+      
+      c(decision_rmix,decision_fb,decision_rmap)
     }
   
-  ttt = data.frame(Prop = colMeans(results), Method = c("JAGS", "RBesT"), TrueCtrlMean = muvec_c[m])
+  ttt = data.frame(Prop = colMeans(results), Method = c("rMix", "FullHier", "rMAP"), TrueCtrlMean = muvec_c[m])
   if(m==1) outdt = ttt else outdt = rbind(outdt,ttt)
   
 }
